984ece25d5fcd7c87b4de0ec1d5d20f0579c4af5
Mathew Topper <damm_horse@yahoo.co.uk>
2023-02-23 21:19:26 +0000

    Keep translating

--- pulumi/index.ts
+++ pulumi/index.ts
@@ -1,25 +1,13 @@
-import * as pulumi from "@pulumi/pulumi";
-import * as aws from "@pulumi/aws";
-import { config } from 'dotenv';
+// Copyright 2016-2019, Pulumi Corporation.  All rights reserved. (Apache 2.0)
+// Modifications copyright (C) 2023 Mathew Topper
 
-function get_domain_and_subdomain(domain: string) {
-    
-  var parts = domain.split(".");
-  
-  if (parts.length < 2) {
-      throw new Error(`No TLD found on ${domain}`);
-  }
-  
-  if (parts.length == 2) {
-    return ['', domain] as const;
-  }
-  
-  const subdomain = parts[0];
-  parts.shift();
-  
-  return [subdomain, parts.join('.') + '.'] as const;
-  
-}
+import * as aws from "@pulumi/aws";
+import * as fs from "fs";
+import * as mime from "mime";
+import * as path from "path";
+import * as pulumi from "@pulumi/pulumi";
+import { config } from 'dotenv';
+import { local } from "@pulumi/command";
 
 const serverPath = process.env.SERVER_PATH;
 const projectPath = process.env.PROJECT_PATH;
@@ -27,6 +15,9 @@ const environment = config({ path: projectPath });
 const memorySize = parseInt(process.env.MEMORY_SIZE!) || 128;
 const [_, zoneName, ...MLDs] = process.env.FQDN?.split('.') || [];
 const domainName = [zoneName, ...MLDs].join(".");
+const staticPath = process.env.STATIC_PATH;
+const prerenderedPath = process.env.PRERENDERED_PATH;
+const routes = process.env.ROUTES?.split(',') || [];
 
 const iamForLambda = new aws.iam.Role("iamForLambda", {assumeRolePolicy: `{
     "Version": "2012-10-17",
@@ -97,11 +88,8 @@ if (process.env.FQDN) {
         validationMethod: 'DNS'},
         { provider: eastRegion });
     
-    const [subdomain,
-           parent_domain] = get_domain_and_subdomain(process.env.FQDN!)
-    
     const hostedZone = aws.route53.getZone({
-            name: parent_domain,
+            name: domainName,
             privateZone: false,
         });
     
@@ -125,10 +113,7 @@ if (process.env.FQDN) {
     
 }
 
-
-
 const bucket = new aws.s3.BucketV2("StaticContentBucket", {tags: {
-    Name: "My bucket",
     forceDestroy: "true",
 }});
 
@@ -137,9 +122,52 @@ const bAcl = new aws.s3.BucketAclV2("bAcl", {
     acl: "private",
 });
 
+// crawlDirectory recursive crawls the provided directory, applying the 
+// provided function to every file it contains. Doesn't handle cycles from
+// symlinks.
+function crawlDirectory(dir: string, f: (_: string) => void) {
+    const files = fs.readdirSync(dir);
+    for (const file of files) {
+        const filePath = `${dir}/${file}`;
+        const stat = fs.statSync(filePath);
+        if (stat.isDirectory()) {
+            crawlDirectory(filePath, f);
+        }
+        if (stat.isFile()) {
+            f(filePath);
+        }
+    }
+}
 
-const originRequestPolicy = new aws.cloudfront.OriginRequestPolicy(
-    "OriginRequestPolicy", {
+// Sync the contents of the source directory with the S3 bucket, which will 
+// in-turn show up on the CDN.
+const webContentsRootPath = path.join(process.cwd(), config.pathToWebsiteContents);
+console.log("Syncing contents from local disk at", webContentsRootPath);
+crawlDirectory(
+    webContentsRootPath,
+    (filePath: string) => {
+        const relativeFilePath = filePath.replace(webContentsRootPath + "/", "");
+        const contentFile = new aws.s3.BucketObjectv2(
+            relativeFilePath,
+            {
+                key: relativeFilePath,
+                acl: "public-read",
+                bucket: bucket.id,
+                contentType: mime.getType(filePath) || undefined,
+                source: new pulumi.asset.FileAsset(filePath),
+            },
+            {
+                parent: bucket,
+            });
+    });
+
+const originAccessIdentity = new aws.cloudfront.OriginAccessIdentity(
+    "OriginAccessIdentity", {
+    comment: "this is needed to setup s3 polices and make s3 not public.",
+});
+
+const defaultRequestPolicy = new aws.cloudfront.OriginRequestPolicy(
+    "DefaultRequestPolicy", {
     cookiesConfig: {
         cookieBehavior: "all",
     },
@@ -163,8 +191,64 @@ const originRequestPolicy = new aws.cloudfront.OriginRequestPolicy(
 });
 
 
+const routeRequestPolicy = new aws.cloudfront.OriginRequestPolicy(
+    "RouteRequestPolicy", {
+    cookiesConfig: {
+        cookieBehavior: "none",
+    },
+    headersConfig: {
+        headerBehavior: "whitelist",
+        headers: {
+            items: [
+                'User-Agent',
+                'Referer'],
+        },
+    },
+    queryStringsConfig: {
+        queryStringBehavior: "none",
+    },
+});
+
+
+function buildBehavior(route: string) {
+    return {
+        pathPattern: route,
+        allowedMethods: [
+            "GET",
+            "HEAD",
+            "OPTIONS",
+        ],
+        cachedMethods: [
+            "GET",
+            "HEAD",
+            "OPTIONS",
+        ],
+        targetOriginId: "s3Origin",
+        originRequestPolicyId: routeRequestPolicy.id,
+        viewerProtocolPolicy: "redirect-to-https",
+    }
+}
+
 const distribution = new aws.cloudfront.Distribution(
     "CloudFrontDistribution", {
+    origins: [{
+        originId: "httpOrigin",
+        domainName: httpApi.apiEndpoint.apply(
+            endpoint => endpoint.split('://')[1]),
+        customOriginConfig: {
+            httpPort: 80,
+            httpsPort: 443,
+            originProtocolPolicy: "https-only",
+            originSslProtocols: ["SSLv3", "TLSv1", "TLSv1.1", "TLSv1.2"]
+        },
+    },
+    {
+        originId: "s3Origin",
+        domainName: bucket.bucketRegionalDomainName,
+        s3OriginConfig: {
+            originAccessIdentity: originAccessIdentity.cloudfrontAccessIdentityPath,
+        },
+    }],
     priceClass: "PriceClass_100",
     enabled: true,
     defaultRootObject: "",
@@ -188,30 +272,75 @@ const distribution = new aws.cloudfront.Distribution(
             "PUT",
         ],
         cachedMethods: [],
-        originRequestPolicyId: originRequestPolicy.id,
-        targetOriginId: "bob"
+        originRequestPolicyId: defaultRequestPolicy.id,
+        targetOriginId: "httpOrigin"
     },
-    origins: [{
-        domainName: bucketV2.bucketRegionalDomainName,
-        originAccessControlId: aws_cloudfront_origin_access_control["default"].id,
-        originId: s3OriginId,
-    }],
+    orderedCacheBehaviors: routes.map(buildBehavior),
     isIpv6Enabled: true,
     comment: "Some comment",
-    loggingConfig: {
-        includeCookies: false,
-        bucket: "mylogs.s3.amazonaws.com",
-        prefix: "myprefix",
-    },
-    aliases: [
-        "mysite.example.com",
-        "yoursite.example.com",
-    ],
     restrictions: {
         geoRestriction: {
             restrictionType: "none",
         },
     },
 });
+
+// Split a domain name into its subdomain and parent domain names.
+// e.g. "www.example.com" => "www", "example.com".
+function getDomainAndSubdomain(domain: string): { subdomain: string, parentDomain: string } {
+    const parts = domain.split(".");
+    if (parts.length < 2) {
+        throw new Error(`No TLD found on ${domain}`);
+    }
+    // No subdomain, e.g. awesome-website.com.
+    if (parts.length === 2) {
+        return { subdomain: "", parentDomain: domain };
+    }
+
+    const subdomain = parts[0];
+    parts.shift();  // Drop first element.
+    return {
+        subdomain,
+        // Trailing "." to canonicalize domain.
+        parentDomain: parts.join(".") + ".",
+    };
+}
+
+// Creates a new Route53 DNS record pointing the domain to the CloudFront distribution.
+function createAliasRecord(
+        targetDomain: string,
+        distribution: aws.cloudfront.Distribution): aws.route53.Record {
+    const domainParts = getDomainAndSubdomain(targetDomain);
+    const hostedZoneId = aws.route53.getZone(
+        {name: domainParts.parentDomain},
+        {async: true}).then(zone => zone.zoneId);
+    return new aws.route53.Record(
+        targetDomain,
+        {
+            name: domainParts.subdomain,
+            zoneId: hostedZoneId,
+            type: "A",
+            aliases: [
+                {
+                    name: distribution.domainName,
+                    zoneId: distribution.hostedZoneId,
+                    evaluateTargetHealth: true,
+                },
+            ],
+        });
+}
+
+if (process.env.FQDN) {
+    const aRecord = createAliasRecord(process.env.FQDN, distribution);
+}
+
+invalidationCommand = new local.Command("invalidate", {
+    create: pulumi.interpolate`aws cloudfront create-invalidation --distribution-id ${distribution.id} --paths '/*'`
+    environment: {
+      ETAG: indexFile.etag
+    }
+  }, {
+      replaceOnChanges: ["environment"]
+  });
 
 // exports.url = counterTable.url;
